fd5858685cf01117747ed6272a687a3c90703535
tianyang <718289248@qq.com>
2019-05-16 15:25:25 +0800

    用于测试提交

--- practice_appium/test_Xq_homework2.py
+++ practice_appium/test_Xq_homework2.py
@@ -7,7 +7,7 @@ import pytest
 from appium.webdriver.common.touch_action import TouchAction
 from appium.webdriver.webdriver import WebDriver
 
-
+# 注释由于测试提交
 class TestXueqiu_homework(object):
     driver = WebDriver
 
