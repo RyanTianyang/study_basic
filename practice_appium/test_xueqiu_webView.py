3ed8ab81535f0fd1c1fd3edf98b69f97a6a61e33
tianyang <718289248@qq.com>
2019-05-22 10:26:51 +0800

    learning practice

--- practice_appium/test_xueqiu_webView.py
+++ practice_appium/test_xueqiu_webView.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from appium.webdriver.common.mobileby import MobileBy
 from appium.webdriver.common.touch_action import TouchAction
 import pytest
 from appium import webdriver
 from appium.webdriver.webdriver import WebDriver
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class Test_swipe(object):
-    driver=WebDriver
+
+class Test_webView(object):
+    driver = WebDriver
 
     @classmethod
     def setup_class(cls):
-        cls.driver=cls.install_app()
-
+        cls.driver = cls.install_app()
 
     def setup_method(self):
-        self.driver=Test_swipe.driver
-        self.driver.find_element_by_xpath("//*[contains(@resource-id,'indicator')]//*[@text='基金']")
+        self.driver = Test_webView.driver
+        WebDriverWait(self.driver, 20, 1).until(EC.presence_of_element_located((MobileBy.XPATH, "//*[@resource-id='android:id/tabs']//*[@text='交易']")))
+        self.driver.find_element_by_xpath("//*[@resource-id='android:id/tabs']//*[@text='交易']").click()
 
-    def test_swipe(self):
-        rect=self.driver.get_window_rect()
-        self.driver.find_element_by_xpath("//*[contains(@resource-id, 'indicator')]//*[@text='基金']").click()
-        time.sleep(2)
-        action_swipe = TouchAction(self.driver)
-        for i in range(5):
-            # action_swipe.press(x=rect['width']*0.2, y=rect['height']*0.8).move_to(x=rect['width']*0.2, y=rect['height']*0.6).release().perform()
-            self.driver.swipe(rect['width']*0.2, rect['height']*0.8,rect['width']*0.2, rect['height']*0.6)
-            time.sleep(2)
+    def test_webView_A(self):
+        WebDriverWait(self.driver, 20, 1).until(EC.presence_of_element_located((MobileBy.ACCESSIBILITY_ID, "A股开户")))
+        self.driver.find_element_by_accessibility_id("A股开户").click()
 
-    def test_webView(self):
-
+    def teardown_method(self):
+        self.driver.back()
 
     @classmethod
     def install_app(cls) -> WebDriver:
@@ -44,5 +42,5 @@ class Test_swipe(object):
             # "unicodeKeyboard": True
         }
         driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(20)
         return driver
